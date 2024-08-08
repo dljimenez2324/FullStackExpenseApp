8c0290333460fa50cf67712269da0a965c91bb0a
dljimenez2324 <dljimenez2324@gmail.com>
2024-08-07 18:02:43 -0700

    added some edits

--- api/Controllers/ExpenseController.cs
+++ api/Controllers/ExpenseController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -10,6 +11,12 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class ExpenseController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public ExpenseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fc3baf9cbf864493ba5a70fe2566d5d53a3336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f0522221ee9fd163b093dbcb2a42c8a11d25bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
