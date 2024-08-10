4d7f4570a1fe4161a3338f0fc82aca4acc2d980e
dljimenez2324 <dljimenez2324@gmail.com>
2024-08-10 15:39:34 -0700

    added edit controller  searched by id and input data based on model
    changed endpoint syntax for streamlined consistency

--- api/Controllers/ExpenseController.cs
+++ api/Controllers/ExpenseController.cs
@@ -20,7 +20,7 @@ namespace api.Controllers
         }
         
         // Get all Data   GetExpense()
-        [HttpGet]
+        [HttpGet("GetAllExpenses")]
         public async Task<IEnumerable<Expense>> GetExpense()
         {
             var expenses = await _context.Expenses.AsNoTracking().ToListAsync();
@@ -28,7 +28,7 @@ namespace api.Controllers
         }
 
         // Add an Expense  Create()
-        [HttpPost]
+        [HttpPost("AddExpense")]
         public async Task<IActionResult> Create (Expense expense)
         {
             if(!ModelState.IsValid)
@@ -47,7 +47,7 @@ namespace api.Controllers
         }
 
         // Delete Expense Item
-        [HttpDelete("{id:int}")]
+        [HttpDelete("Delete/{id:int}")]
         public async Task<IActionResult> DeleteExpense(int id)
         {
             var expense = await _context.Expenses.FindAsync(id);
@@ -66,6 +66,38 @@ namespace api.Controllers
             }
 
             return BadRequest("Unable to delete expense item");
+        }
+
+        // Edit Expense Item by id
+        [HttpPut("Edit/{id:int}")]
+        public async Task<IActionResult> EditExpenseItem(int id, Expense expense)
+        {
+            // save the expense from the database found by id
+            var expenseFromDb = await _context.Expenses.FindAsync(id);
+
+            // check if the database with that id is empty & return badrequest
+            if(expenseFromDb == null)
+            {
+                return BadRequest("Expense not found");  
+            }
+
+            // if expense at id is not empty then update data based upon what is taken as input
+            expenseFromDb.Description = expense.Description;
+            expenseFromDb.Amount = expense.Amount;
+            expenseFromDb.Category = expense.Category;
+
+            // save changes to the variable result
+            var result = await _context.SaveChangesAsync();
+
+            // if result is not empty then return Ok
+            if(result > 0)
+            {
+                return Ok("Expense information updated for " + expense.Description);
+            }
+
+            // if result is empty return BadRequest with message
+            return BadRequest("Unable to update the expense item " + expense.Description);
+
         }
     }
 }
